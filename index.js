361c635b854e53577bf445dadf9a1b43e8dda2a2
felluffy <somerandomghai@gmail.com>
2018-08-03 17:28:58 +0600

    basic resturcturing of the project

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const venues = require('./routes/venues');
+const events = require('./routes/events');
+const orgranizers = require('./routes/organizers');
+const periods = require('./routes/periods');
 const mongoose = require('mongoose');
 const express = require('express');
-const Joi = require('joi');
 const app = express();
+
+//import file and use it or implement here
+app.use('/api/venues', venues);
+app.use('/api/organizers', orgranizers);
+app.use('/api/periods', periods);
+app.use('/api/events', events);
 
 mongoose.connect('mongodb://localhost/cultural_uni')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
-
-//import file and use it or implement here
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
