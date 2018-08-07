fc6b37f13d2b4cf322d7c6a06da91fe7cc2a65bd
JOYRU <joycseru@gmail.com>
2018-08-07 22:32:07 +0600

    add post operation for login in users root

--- routes/users.js
+++ routes/users.js
@@ -30,3 +30,11 @@ router.post('/signup', (req, res, next) => {
     }
   });
 });
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
