a56bd21c9d684e7cdda2e656ea8ed02bc532ce14
JOYRU <joycseru@gmail.com>
2018-08-07 22:34:02 +0600

    write logout route in users endpoint

--- routes/users.js
+++ routes/users.js
@@ -38,3 +38,23 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
+
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+});
+
+
+
+
+
+module.exports = router;
